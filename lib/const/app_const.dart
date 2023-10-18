39f851d4d5590b91df42dbd1173daf5ca09519c1
shamsudheen <ptsdeen2007@gmail.com>
2023-10-18 09:18:13 +0530

    removed app const

--- lib/const/app_const.dart
+++
@@ -1,4 +0,0 @@
-class AppConst{
-  static String GOOGLE_MAP_API_KEY="AIzaSyDYXVADat0SYf22g4-eUjtKpWidTozxzC0";
-  // static String GOOGLE_MAP_API_KEY = "AIzaSyClqBNJ2uahJQwyOwxxCKkgdbT4Rh2b_uY";//current
-}
